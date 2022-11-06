35c33c4899d3b081935ce8bc117664e0b72b1e9a
Samiel19 <anton.sadagov@gmail.com>
2022-11-06 17:30:47 +0300

    промежуточная вресия

--- homework.py
+++ homework.py
@@ -11,15 +11,21 @@ class Training:
                  duration: float,
                  weight: float,
                  ) -> None:
-        pass
+        self.action = action
+        self.duration = duration
+        self.weight = weight
+        
 
     def get_distance(self) -> float:
         """Получить дистанцию в км."""
-        pass
+        distance = self.action * self.LEN_STEP / self.M_IN_KM 
+        return distance
+
 
     def get_mean_speed(self) -> float:
         """Получить среднюю скорость движения."""
-        pass
+        speed = Training.get_distance(self)/ self.duration
+        return speed
 
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
@@ -32,17 +38,41 @@ class Training:
 
 class Running(Training):
     """Тренировка: бег."""
-    pass
+    def __init__(self, action: int, duration: float, weight: float):
+        super().__init__(action, duration, weight)
+        self.LEN_STEP = 0.65
+        self.M_IN_KM = 1000
+        self.CALORIES_MEAN_SPEED_MULTIPLIER = 18
+        self.CALORIES_MEAN_SPEED_SHIFT = 1.79 
+
+
+    def get_spent_calories(self):
+        calories = (self.CALORIES_MEAN_SPEED_MULTIPLIER * super().get_mean_speed() + self.CALORIES_MEAN_SPEED_SHIFT) * self.weight / self.M_IN_KM * self.duration
+        return calories
+
+    
 
 
 class SportsWalking(Training):
     """Тренировка: спортивная ходьба."""
-    pass
+    def __init__(self, action: int, duration: float, weight: float):
+        super().__init__(action, duration, weight)
+        self.action = action
+        self.duration = duration
+        self.weight = weight
+        self.LEN_STEP = 0.65
+        self.M_IN_KM = 1000
 
 
 class Swimming(Training):
     """Тренировка: плавание."""
-    pass
+    def __init__(self, action: int, duration: float, weight: float):
+        super().__init__(action, duration, weight)
+        self.action = action
+        self.duration = duration
+        self.weight = weight
+        self.LEN_STEP = 1.38
+        self.M_IN_KM = 1000
 
 
 def read_package(workout_type: str, data: list) -> Training:
@@ -66,3 +96,7 @@ if __name__ == '__main__':
         training = read_package(workout_type, data)
         main(training)
 
+train = Running(15000, 1, 75)
+print(train.get_distance())
+print(train.get_mean_speed())
+print(train.get_spent_calories())
